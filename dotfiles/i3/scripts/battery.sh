6ae28af3377232860d8499af989dfc8ef0f099ff
nubbsterr <sherm5344@gmail.com>
2025-09-21 17:59:25 -0400

    battery script updated to work

--- dotfiles/i3/scripts/battery.sh
+++ dotfiles/i3/scripts/battery.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-echo " $(\cat /sys/class/power_supply/BAT0/capacity)% "
+BAT0_PATH="/sys/class/power_supply/BAT0/capacity"
+BAT1_PATH="/sys/class/power_supply/BAT1/capacity"
+
+bat0=$(\cat "$BAT0_PATH" 2>/dev/null || echo 0)
+bat1=$(\cat "$BAT1_PATH" 2>/dev/null || echo 0)
+
+avg=$(( (bat0 + bat1) / 2 ))
+echo " $avg% "
